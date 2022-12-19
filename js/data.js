25aab37d9954d4495a66619929055de5b1093ed4
Olga <olg200919@ya.ru>
2022-12-19 20:38:21 +0300

    Задание

---
+++ js/data.js
@@ -0,0 +1,31 @@
+const jokeData = `
+[
+    {
+        "id": 204,
+        "type": "general",
+        "setup": "What do you call a boomerang that won't come back?",
+        "punchline": "A stick.",
+        "image_help": "https://avatars.mds.yandex.net/i?id=2a0000018466e4001d1c35c421dc0e39ca69-1531182-fast-images&n=13"
+      },
+      {
+        "id": 227,
+        "type": "general",
+        "setup": "What do you call a sheep with no legs?",
+        "punchline": "A cloud.",
+        "image_help": "https://avatars.mds.yandex.net/i?id=ccc2da1cd0c329b77db12c3636f5a738-5274396-images-thumbs&n=13"
+      },
+      {
+        "id": 76,
+        "type": "general",
+        "setup": "Never take advice from electrons.",
+        "punchline": "They are always negative.",
+        "image_help":"https://avatars.mds.yandex.net/i?id=4bddd999ca7c01665ee404c9d8ef9069-5288364-images-thumbs&n=13"
+      },
+      {
+        "id": 276,
+        "type": "general",
+        "setup": "What was a more important invention than the first telephone?",
+        "punchline": "The second one.",
+        "image_help": "https://cdn.cellfaktor.ru/images/tovar/1303_1.jpg"
+      }
+]`
